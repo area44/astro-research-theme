bbdae46cb3da123f1e6e2d76ecd4e31621fe7c08
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-09-04 17:06:53 +0700

    Refactor BaseHead component for improved SEO and fallback handling; remove index.astro and create Header component for better structure; migrate content to index.mdx with updated layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 const site = process.env.SITE || "localhost://4321";
@@ -6,4 +7,13 @@ const base = process.env.BASE || "/";
 export default defineConfig({
   site,
   base,
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
 });
